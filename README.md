ec898c5eef2b317bb88af9db7ebf5c9d9da73507
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-09-15 19:57:08 +0200

    u

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Install
+```bash
+pip install git+https://github.com/phuvinhnguyen/BotSync.git
+```
+
+## Gemini
+```python
+from BotSync.gemini import GeminiBot
+
+bot = GeminiBot('AIzaSyAbcdKGvTrURIW5QneMmjEYJb61xp571Sg')
+
+bot.generate("hello")
+```
--- setup.py
+++ setup.py
@@ -5,24 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="MedicalVision",
+    name="BotSync",
     version="0.1.0",
     description=long_description,
     packages=find_packages(),  # Automatically find packages in your project
     install_requires=[
         "torch>=1.9.0",  # PyTorch for deep learning
         "torchvision>=0.10.0",  # torchvision for image processing utilities
-        "Pillow>=8.2.0",  # Image processing library
-        "numpy>=1.19.2",  # Array processing library
-        "matplotlib>=3.3.4",  # Plotting library
-        "opencv-python>=4.5.1.48",  # Computer vision library
-        "lightning>=2.3.3",
-        "coco-eval",
-        "funcy",
-        "wandb",
+        ""
     ],
-    package_data={
-        'MedicalVision': ['preprocess/*'],
-    },
     include_package_data=True,  # Include data files specified in package_data
 )
