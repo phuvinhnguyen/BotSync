62062587e59d7789ea7f5f333da6f7902129113f
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2024-09-27 09:13:34 +0200

    u: using multiple gemini tokens

--- BotSync/gemini.py
+++ BotSync/gemini.py
@@ -6,11 +6,27 @@ from .monitor import AbstractBot
 class GeminiBot(AbstractBot):
     def __init__(self, token, bot="gemini-1.5-flash"):
         super().__init__()
-        genai.configure(api_key=token)
-        self.model = genai.GenerativeModel(bot)
+        self.bot = bot
+        self.token = token
+
+        if isinstance(token, list):
+            self.switch_between = True
+            self.index = 0
+            self.num_token = len(token)
+            self.repeat_time = len(token) + 2
+        else:
+            genai.configure(api_key=token)
+            self.switch_between = False
+            self.model = genai.GenerativeModel(bot)
+            self.repeat_time = 5
 
     def __run__(self, text):
-        for _ in range(5):
+        for _ in range(self.repeat_time):
+            if self.switch_between:
+                self.index = (self.index + 1) % self.num_token
+                genai.configure(api_key=self.token[self.index])
+                self.model = genai.GenerativeModel(bot=self.bot)
+
             try:
                 result = self.model.generate_content(text).text
                 return result
